ea9a3dd9dfedca063b49b953bbd36859b0aa37eb
Anuj Tomar <114133251+anujtomar1201@users.noreply.github.com>
2024-06-29 15:54:35 +0530

    Update firebase.js

--- personal-finance-tracker-main/src/firebase.js
+++ personal-finance-tracker-main/src/firebase.js
@@ -3,7 +3,20 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
+
+
 const firebaseConfig = {
+  apiKey: "AIzaSyCL5CjNzUrtp0kWB9imH9_DispgRyVu-kI",
+  authDomain: "myfinanceapp-94ad8.firebaseapp.com",
+  projectId: "myfinanceapp-94ad8",
+  storageBucket: "myfinanceapp-94ad8.appspot.com",
+  messagingSenderId: "997981445040",
+  appId: "1:997981445040:web:3e8ec5675fc6305098d475",
+  measurementId: "G-79B7DV3MF2"
+};
+
+
+/*const firebaseConfig = {
   apiKey: "AIzaSyAHExHfMDRu2lVecgYxgW86TXDF-dvsWrY",
   authDomain: "personal-finance-tracker-ec56e.firebaseapp.com",
   projectId: "personal-finance-tracker-ec56e",
@@ -12,7 +25,7 @@ const firebaseConfig = {
   appId: "1:669303047696:web:5b4b41cc896752c03e6c95",
   measurementId: "G-7X36E69HYW",
 };
-
+*/
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
