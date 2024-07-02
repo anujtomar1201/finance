bfdbc042b7eab902027b174e2fb0e204b6174898
Anuj Tomar <114133251+anujtomar1201@users.noreply.github.com>
2024-07-02 16:57:24 +0530

    Update firebase.js

--- personal-finance-tracker-main/src/firebase.js
+++ personal-finance-tracker-main/src/firebase.js
@@ -4,9 +4,6 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 
-
-
-/*
 const firebaseConfig = {
   apiKey: "AIzaSyAHExHfMDRu2lVecgYxgW86TXDF-dvsWrY",
   authDomain: "personal-finance-tracker-ec56e.firebaseapp.com",
@@ -15,18 +12,6 @@ const firebaseConfig = {
   messagingSenderId: "669303047696",
   appId: "1:669303047696:web:5b4b41cc896752c03e6c95",
   measurementId: "G-7X36E69HYW",
-};
-
-*/
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCL5CjNzUrtp0kWB9imH9_DispgRyVu-kI",
-  authDomain: "myfinanceapp-94ad8.firebaseapp.com",
-  projectId: "myfinanceapp-94ad8",
-  storageBucket: "myfinanceapp-94ad8.appspot.com",
-  messagingSenderId: "997981445040",
-  appId: "1:997981445040:web:3e8ec5675fc6305098d475",
-  measurementId: "G-79B7DV3MF2"
 };
 
 
